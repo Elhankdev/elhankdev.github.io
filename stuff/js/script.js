c0c867559142920f34eb5f4e3cb7bb94c46717c3
Henk <98973891+Elhankdev@users.noreply.github.com>
2022-02-12 17:04:23 +0100

    Stuff uploaded

---
+++ stuff/js/script.js
@@ -0,0 +1,13 @@
+
+function clickonlogo() {
+    //var=variablen die 
+    var AlertText ='Wuff Wuff Wuffidi Wufff';
+    alert(AlertText);
+}
+
+ function ondogclick() {
+    document.getElementById("hundebild").style.width = "1500px";
+    document.getElementById("hundebild").style.transform = "rotate(10deg)";
+    document.getElementById("hundebild").style.cursor = "grab";
+    document.getElementById("hundebild").style.boxShadow = "0 0 10px rgba(41, 109, 176, 1)";
+ }
